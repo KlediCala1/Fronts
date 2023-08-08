43e5048c133ff3e253e019097d352bdd57ac866a
Kledi Cala <kledicala1@gmail.com>
2023-08-08 19:38:51 +0200

    Initial commit

---
+++ src/main/java/com/java/fronts/FrontsApplication.java
@@ -0,0 +1,13 @@
+package com.java.fronts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrontsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FrontsApplication.class, args);
+    }
+
+}
